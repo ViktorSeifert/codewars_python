3ecfe016f31e27989c45cee37c96ad48cdbc0517
Viktor Seifert <viktor.seifert@kimeta.de>
2018-06-04 17:41:02 +0200

    Kata: Number of Proper Fractions

---
+++ src/excercises/Number_of_Proper_Fractions.py
@@ -0,0 +1,31 @@
+def gcd(a, b):
+    if b == 0:
+        return a
+
+    return gcd(b, a % b)
+
+
+def proper_fractions(n):
+    if n == 1:
+        return 0
+
+    result = {1: True}
+
+    for i in range(2, n):
+        if i in result.keys():
+            continue
+
+        if gcd(n, i) == 1:
+            result[i] = True
+        else:
+            result[i] = False
+
+            for j in range(2, n // i):
+                result[i * j] = False
+
+    return len([x for x in result.items() if x[1]])
+
+
+print(proper_fractions(15))
+print(proper_fractions(25))
+print(proper_fractions(2500000))
