bab6b95a3d176e5686b9c594c6e5fbbf1be8c4b5
Viktor Seifert <viktor.seifert@kimeta.de>
2018-06-18 16:10:27 +0200

    Kata: Number of Proper Fractions, switched to naive prime factors implementation

--- src/excercises/Number_of_Proper_Fractions.py
+++ src/excercises/Number_of_Proper_Fractions.py
@@ -1,3 +1,37 @@
+from __future__ import print_function, division
+
+
+def prime_numbers(num):
+    primes = [True] * (num + 1)
+    primes[0] = False
+    primes[1] = False
+
+    for i in range(2, num + 1):
+        if primes[i]:
+            for k in range(i + i, num + 1, i):
+                primes[k] = False
+
+    result = []
+
+    for i, prime in enumerate(primes):
+        if prime and i > 0:
+            result.append(i)
+
+    return result
+
+
+def prime_factors(n):
+    primes = prime_numbers(n)
+
+    factors = []
+
+    for i in primes:
+        if gcd(n, i) > 1:
+            factors.append(i)
+
+    return factors
+
+
 def gcd(a, b):
     while b != 0:
         a, b = b, a % b
@@ -9,19 +43,18 @@ def proper_fractions(n):
     if n == 1:
         return 0
 
-    proper = set()
-    proper.add(1)
+    result = 1.
 
-    for i in range(2, n):
-        if n % i == 0:
-            continue
+    for factor in prime_factors(n):
+        part = (1 - (1 / factor))
+        result *= part
 
-        if gcd(n, i) == 1:
-            proper.add(i)
-
-    return len(proper)
+    return int(result * n)
 
 
+print(proper_fractions(1))
+print(proper_fractions(2))
+print(proper_fractions(5))
 print(proper_fractions(15))
 print(proper_fractions(25))
 print(proper_fractions(2500000))
