64e1a92c32b9249f2c839aab73e76dd24927700f
Viktor Seifert <viktor@viktor-s.net>
2018-12-24 00:16:34 +0100

    'Is sorted and how' kata

--- clojure/src/kata/is_sorted_and_how.clj
+++ clojure/src/kata/is_sorted_and_how.clj
@@ -1,4 +1,18 @@
 (ns kata.is-sorted-and-how)
 
 (defn sorted-and-how? [sq]
-  "")
+  (let [lt-or-gt
+    (->>
+      sq
+      (partition 2 1)
+      (map #(cond
+              (< (first %) (second %)) :lt
+              (> (first %) (second %)) :gt
+              ; Special treatment of eq
+              ; because the kata tests
+              ; see [9 9 10] as not being sorted.
+              :else :eq)))]
+    (cond
+      (every? #(= :lt %) lt-or-gt) "yes, ascending"
+      (every? #(= :gt %) lt-or-gt) "yes, descending"
+      :else "no")))
--- clojure/test/kata/is_sorted_and_how_testing.clj
+++ clojure/test/kata/is_sorted_and_how_testing.clj
@@ -6,4 +6,5 @@
   (are [inp exp] (= exp (solution inp))
        [1 2] "yes, ascending"
        [15 7 3 -8] "yes, descending"
-       [4 2 30] "no"))
+       [4 2 30] "no"
+       [9 9 41 65 75 78 129 184 195] "no"))
