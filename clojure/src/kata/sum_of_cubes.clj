60e9ea74372391a37a528855bf176c47497039a7
Viktor Seifert <viktor@viktor-s.net>
2018-12-24 21:02:27 +0100

    Kata 'Sum of cubes', faster but still too slow

--- clojure/src/kata/sum_of_cubes.clj
+++ clojure/src/kata/sum_of_cubes.clj
@@ -3,23 +3,20 @@
 (defn- pow [x y] (reduce * (repeat y x)))
 
 (defn- cube-sum [n]
-  (->>
-    (range 1 (+ n 1))
-    (map #(pow % 3))
-    (reduce +)))
+  (try
+    (->>
+      (range 1 (+ n 1))
+      (map #(pow % 3))
+      (reduce +))
+    (catch ArithmeticException ex -1)))
 
-(defn- find-nb-down [m n]
-  (loop [n n]
+(defn- find-nb-impl [m initial-step]
+  (loop [n 1 step initial-step]
     (let [cs (cube-sum n)]
       (cond
+        (< step 1) -1
         (= m cs) n
-        (< m cs) (recur (- n 1))
-        :else -1))))
+        (> m cs) (recur (+ n step) step)
+        (< m cs) (recur (- n step) (/ step 10))))))
 
-(defn find-nb [m]
-  (loop [n 1]
-    (let [cs (cube-sum n)]
-      (cond
-        (= m cs) n
-        (> m cs) (recur (* n 2))
-        :else (trampoline find-nb-down m n)))))
+(defn find-nb [m] (find-nb-impl m 10000))
