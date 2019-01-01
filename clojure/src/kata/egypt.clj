bd2fb2d01934c47f32616a8a073d4cba3be19268
Viktor Seifert <viktor@viktor-s.net>
2019-01-01 18:40:43 +0100

    Kata 'Some Egyptian fractions', working but still too slow

--- clojure/src/kata/egypt.clj
+++ clojure/src/kata/egypt.clj
@@ -3,12 +3,12 @@
 ; https://www.codewars.com/kata/54f8693ea58bce689100065f/train/clojure
 
 (defn- decompose-impl [n denom decomp]
-  (if (= n (reduce + decomp))
+  (if (zero? n)
     (filter pos? decomp)
-    (let [expanded-decomp (conj decomp (/ 1 denom))
-          current-sum (reduce + expanded-decomp)]
-      (if (<= current-sum n)
-        (recur n (inc denom) expanded-decomp)
+    (let [fraction (/ denom)]
+      ; (<= fraction n) should work, but it seems slower that what is below
+      (if (not (neg? (- n fraction)))
+        (recur (- n fraction) (inc denom) (conj decomp fraction))
         (recur n (inc denom) decomp)))))
 
 (defn decompose [r]
@@ -16,4 +16,4 @@
         whole-part (int r)]
     (if (zero? r)
       [] 
-      (->> (decompose-impl r 2 [whole-part]) (map str)))))
+      (->> (decompose-impl (- r whole-part) 2 [whole-part]) (map str)))))
