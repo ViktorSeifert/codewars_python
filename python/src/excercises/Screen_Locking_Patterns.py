74da94136f8881bb275703db3e15881b1b725038
Viktor Seifert <viktor.seifert@kimeta.de>
2019-03-14 17:26:33 +0100

    Kata: Screen Locking Patterns, working but slow solution

--- python/src/excercises/Screen_Locking_Patterns.py
+++ python/src/excercises/Screen_Locking_Patterns.py
@@ -25,17 +25,65 @@ point_map = [
     ["D", "E"],
     ["D", "E", "F"],
     ["D", "G"],
-    ["D", "E"],
+    ["D", "H"],
+    ["D", "I"],
+    ["E", "F"],
+    ["E", "G"],
+    ["E", "H"],
+    ["E", "I"],
+    ["F", "G"],
+    ["F", "H"],
+    ["F", "I"],
+    ["G", "H"],
+    ["G", "H", "I"],
+    ["H", "I"],
 ]
 
+
+def all_direct_paths_from(point):
+    for p in point_map:
+        if p[0] == point:
+            yield p
+        elif p[-1] == point:
+            p = p[:]
+            p.reverse()
+            yield p
+
+
 def count_patterns_from(firstPoint, length):
-    pass
+    if length <= 0:
+        return 0
+    patterns = []
+    patterns_recur([firstPoint], length - 1, patterns)
+    return len(patterns)
 
 
-print(count_patterns_from('A',10), 0)
-print(count_patterns_from('A',0),  0)
-print(count_patterns_from('E',14), 0)
-print(count_patterns_from('B',1),  1)
-print(count_patterns_from('C',2),  5)
-print(count_patterns_from('E',2),  8)
-print(count_patterns_from('E',4),  256)
+def concat(pattern, path):
+    new_pattern = pattern[:]
+    for p in path:
+        if p not in new_pattern:
+            new_pattern.append(p)
+    return new_pattern
+
+
+def patterns_recur(pattern, length, patterns):
+    if length < 0:
+        return
+
+    if length == 0 and pattern not in patterns:
+        patterns.append(pattern)
+        return
+
+    for path in all_direct_paths_from(pattern[-1]):
+        new_pattern = concat(pattern, path)
+        if new_pattern != pattern:
+            patterns_recur(new_pattern, length - (len(new_pattern) - len(pattern)), patterns)
+
+
+print(count_patterns_from('A', 10), 0)
+print(count_patterns_from('A', 0), 0)
+print(count_patterns_from('E', 14), 0)
+print(count_patterns_from('B', 1), 1)
+print(count_patterns_from('C', 2), 5)
+print(count_patterns_from('E', 2), 8)
+print(count_patterns_from('E', 4), 256)
