809e581f972d2e2e2aa9585dd14a117ae1002204
Viktor Seifert <viktor.seifert@kimeta.de>
2019-04-17 16:02:11 +0200

    Kata: Eggs crush test skeleton

--- python/src/excercises/Fabergè_Easter_Eggs_crush_test.py
+++ python/src/excercises/Fabergè_Easter_Eggs_crush_test.py
@@ -1,1 +1,19 @@
 # https://www.codewars.com/kata/54cb771c9b30e8b5250011d4/train/python
+# this is unsolved
+
+
+def height(n, m):
+    if n == 0 or m == 0:
+        return 0
+
+    if m == 1:
+        return 1
+
+    if n == 1:
+        return 1
+
+    return (n + 1) * m
+
+
+print(height(2, 14))
+print(height(7, 20))
