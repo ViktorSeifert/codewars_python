9b2da7be4d7415120dcf1ed5ba849c6d09073f54
Viktor Seifert <viktor.seifert@kimeta.de>
2019-05-10 18:15:27 +0200

    Kata Permutations, speed improvement WIP

--- haskell/src/KataPermutations.hs
+++ haskell/src/KataPermutations.hs
@@ -1,18 +1,24 @@
 -- https://www.codewars.com/kata/5254ca2719453dcc0b00027d/train/haskell
 
 module KataPermutations where
-  import Data.List (nub, delete)
-  allPermutations :: String -> [String]
-  allPermutations (x:[]) = [x:[]]
-  allPermutations xs = 
-    concat $
-      map (\element ->
-        let
-          allPermutationsWithoutElement = allPermutations $ delete element xs
-        in
-          map (element :) allPermutationsWithoutElement
-      ) xs
+  import Data.List (sort)
+
+  indexOfElementSmallerThanSuccessor :: String -> Int
+  indexOfElementSmallerThanSuccessor s = 
+    indexOfElementSmallerThanSuccessorImpl s 0 where
+      indexOfElementSmallerThanSuccessorImpl (x:[]) _ = -1
+      indexOfElementSmallerThanSuccessorImpl (x:y:xs) n =
+        if x < y then n
+          else indexOfElementSmallerThanSuccessorImpl (y:xs) (n+1)
+
+  -- https://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order
+  nextPermutation :: String -> String
+  nextPermutation = error "TODO"
+
+  permutationsFrom :: String -> [String]
+  permutationsFrom xs | xs == nextPermutation xs = [xs]
+  permutationsFrom xs = xs : [nextPermutation xs]
 
   permutations :: String -> [String]
   permutations "" = [""]
-  permutations s = nub $ allPermutations s
+  permutations s = permutationsFrom $ sort s
