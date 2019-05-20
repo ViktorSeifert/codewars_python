8d8dbd4b6b490c5d0fa34e50556b7bda57c409af
Viktor Seifert <viktor.seifert@kimeta.de>
2019-05-20 16:59:37 +0200

    Kata BefungeInterpreter, implemented arithmetik and moves

--- haskell/src/KataBefungeInterpreter.hs
+++ haskell/src/KataBefungeInterpreter.hs
@@ -2,26 +2,69 @@
 
 module KataBefungeInterpreter where
   import System.Random (StdGen, next)
+  import Data.Char (digitToInt)
 
-  currentInstruction :: (Int, Int) -> [String] -> Char
-  currentInstruction (x,y) instructions = instructions !! x !! y
-
-  nextPosition :: StdGen -> (Int, Int) -> [String] -> (Int, Int)
-  nextPosition rng pos@(x,y) instructions =
+  runBF :: StdGen -> (Int, Int) -> [String] -> [Int] -> String
+  runBF rng pos@(x,y) instructions stack = 
     let
       i = currentInstruction pos instructions
-    in
-      undefined
-
-  runBF :: StdGen -> (Int, Int) -> [Int] -> [String] -> String
-  runBF rng pos@(x,y) stack instructions = 
-    let
-      i = currentInstruction pos instructions
-      nextPos = nextPosition rng pos instructions
+      (nextX, nextY, nextRng) = nextPosition rng pos instructions
+      runIt = runBF nextRng (nextX, nextY) instructions
     in
       case i of
         '@' -> concat $ map show stack
-        '0' -> runBF rng nextPos (0 : stack) instructions
+        i | i `elem` "012345689" -> runIt (digitToInt i : stack)
+        '+' -> runIt (performAdd stack)
+        '-' -> runIt (performSub stack)
+        '*' -> runIt (performMult stack)
+        '/' -> runIt (performDiv stack)
+        '%' -> runIt (performMod stack)
+        '!' -> runIt (performNot stack)
+        '`' -> runIt (performGt stack)
+
+  currentInstruction :: (Int, Int) -> [String] -> Char
+  currentInstruction (x,y) instructions = instructions !! y !! x
+
+  nextPosition :: StdGen -> (Int, Int) -> [String] -> (Int, Int, StdGen)
+  nextPosition rng pos@(x,y) instructions =
+    let
+      i = currentInstruction pos instructions
+      (randN, nextRng) = next rng
+      (deltaX, deltaY) = move randN i
+      nextX = (x + deltaX) `rem` (length (instructions !! 0))
+      nextY = (x + deltaY) `rem` (length instructions)
+    in
+      (nextX, nextY, nextRng)
+
+  move :: Int -> Char -> (Int, Int)
+  move _ '^' = (0, -1)
+  move _ '<' = (-1, 0)
+  move _ '>' = (1, 0)
+  move _ 'v' = (0, 1)
+  move rand '?' = move rand $ "<>^v" !! rand
+  move r _ = move r '>'
+
+  performAdd :: [Int] -> [Int]
+  performAdd (x:y:xs) = x + y : xs
+
+  performSub :: [Int] -> [Int]
+  performSub (x:y:xs) = y - x : xs
+
+  performMult :: [Int] -> [Int]
+  performMult (x:y:xs) = x * y : xs
+
+  performDiv :: [Int] -> [Int]
+  performDiv (x:y:xs) = y `quot` x : xs
+
+  performMod :: [Int] -> [Int]
+  performMod (x:y:xs) = y `rem` x : xs
+
+  performNot :: [Int] -> [Int]
+  performNot (0:xs) = 1 : xs
+  performNot (1:xs) = 0 : xs
+
+  performGt :: [Int] -> [Int]
+  performGt (x:y:xs) = if y > x then 1:xs else 0:xs
 
   interpret :: StdGen -> String -> String
-  interpret rng instructions = runBF rng (0, 0) [] (lines instructions)
+  interpret rng instructions = runBF rng (0, 0) (lines instructions) []
