8020574161c0b17f6159e5d45c705d7da5274456
Viktor Seifert <viktor.seifert@kimeta.de>
2019-05-21 16:09:08 +0200

    Kata BefungeInterpreter, implemented some stack ops

--- haskell/src/KataBefungeInterpreter.hs
+++ haskell/src/KataBefungeInterpreter.hs
@@ -4,23 +4,28 @@ module KataBefungeInterpreter where
   import System.Random (StdGen, next)
   import Data.Char (digitToInt)
 
-  runBF :: StdGen -> (Int, Int) -> [String] -> [Int] -> String
-  runBF rng pos@(x,y) instructions stack = 
+  runBF :: StdGen -> (Int, Int) -> [String] -> String -> [Int] -> String
+  runBF rng pos@(x,y) instructions output stack = 
     let
       i = currentInstruction pos instructions
       (nextX, nextY, nextRng) = nextPosition rng pos instructions
-      runIt = runBF nextRng (nextX, nextY) instructions
+      continueWithOutputAndStack = runBF nextRng (nextX, nextY) instructions
+      continueWithStack = continueWithOutputAndStack output
     in
       case i of
-        '@' -> concat $ map show stack
-        i | i `elem` "012345689" -> runIt (digitToInt i : stack)
-        '+' -> runIt (performAdd stack)
-        '-' -> runIt (performSub stack)
-        '*' -> runIt (performMult stack)
-        '/' -> runIt (performDiv stack)
-        '%' -> runIt (performMod stack)
-        '!' -> runIt (performNot stack)
-        '`' -> runIt (performGt stack)
+        '@' -> output
+        i | i `elem` "012345689" -> continueWithStack (digitToInt i : stack)
+        '+' -> continueWithStack (performAdd stack)
+        '-' -> continueWithStack (performSub stack)
+        '*' -> continueWithStack (performMult stack)
+        '/' -> continueWithStack (performDiv stack)
+        '%' -> continueWithStack (performMod stack)
+        '!' -> continueWithStack (performNot stack)
+        '`' -> continueWithStack (performGt stack)
+        ':' -> continueWithStack (performDup stack)
+        '\\' -> continueWithStack (performSwap stack)
+        '$' -> continueWithStack (tail stack)
+        '.' -> continueWithOutputAndStack (output ++ show (head stack)) (tail stack)
 
   currentInstruction :: (Int, Int) -> [String] -> Char
   currentInstruction (x,y) instructions = instructions !! y !! x
@@ -66,5 +71,13 @@ module KataBefungeInterpreter where
   performGt :: [Int] -> [Int]
   performGt (x:y:xs) = if y > x then 1:xs else 0:xs
 
+  performDup :: [Int] -> [Int]
+  performDup [] = [0]
+  performDup (x:xs) = x:x:xs
+
+  performSwap :: [Int] -> [Int]
+  performSwap (x:[]) = 0:x:[]
+  performSwap (x:y:xs) = y:x:xs
+
   interpret :: StdGen -> String -> String
-  interpret rng instructions = runBF rng (0, 0) (lines instructions) []
+  interpret rng instructions = runBF rng (0, 0) (lines instructions) "" []
